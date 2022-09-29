cb4eb3ed5a9d9d33e232cd764bca9121d5ec6404
Matheus Monteiro <matheuschran@hotmail.com>
2022-09-29 14:06:11 -0300

    realiza o setup inicial do projeto

---
+++ app/backend/src/database/migrations/create-matches.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      home_team: {
+        type: Sequelize.INTEGER,
+      },
+      home_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      away_team: {
+        type: Sequelize.INTEGER,
+      },
+      away_team_goals: {
+        type: Sequelize.INTEGER,
+      },
+      in_progress: {
+        type: Sequelize.BOOLEAN,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('matches');
+  },
+};
